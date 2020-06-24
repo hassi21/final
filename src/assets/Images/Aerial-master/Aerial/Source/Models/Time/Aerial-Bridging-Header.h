f0156ab97b1b02001b737d1171dadf204d8d0ea8
root <root@KingKoreAlpha.Dlink>
2020-06-25 01:27:18 +0500

    My first Commit before changing the home screen layout

--- src/assets/Images/Aerial-master/Aerial/Source/Models/Time/Aerial-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-// We need this to be able to use IOPowerSources for battery status detection in Swift
-#import <IOKit/ps/IOPowerSources.h>
